8583b062daa63079f9e298c9c4c924ba726b316a
TeaChoco <thannateetibmerit@gmail.com>
2026-05-01 18:06:22 +0700

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Providers from './components/layout/Providers';
+import env from './secure/env';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Providers>
-            <BrowserRouter>
+            <BrowserRouter basename={env.BASE}>
                 <App />
             </BrowserRouter>
         </Providers>
